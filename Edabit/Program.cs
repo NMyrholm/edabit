a2dd8047f2ed8532e73036ea59b06815c8decee6
Nils Myrholm <nilsmyrholm@gmail.com>
2019-03-25 13:37:04 +0100

    RemoveVowels
    https://edabit.com/challenge/HXsSRtx83xj7K94dW

---
+++ Edabit/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Edabit
+{
+    public class Edabit
+    {
+        public static void Main(string[]args)
+        {
+            RemoveVowel.Vowel("Hej jag heter Nils");
+        }
+    }
+}
---
+++ Edabit/RemoveVowel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Edabit
+{
+    class RemoveVowel
+    {
+        public static void Vowel(string args)
+        {
+            //string str = "Hej jag heter Nils";
+            Console.WriteLine(SilenceTrump(args));
+        }
+
+        public static string SilenceTrump(string str)
+        {
+            return Regex.Replace(str, "[aeiouAEIOU]", "");
+        }
+    }
+}
